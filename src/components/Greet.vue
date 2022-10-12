b9989936a4d89544cbcfb75b6b4fe73a2c9b5ce7
linzihang <nordglede@foxmail.com>
2022-10-13 01:15:11 +0800

    first commit

---
+++ src/components/Greet.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { invoke } from "@tauri-apps/api/tauri";
+import { ref } from "vue";
+
+const greetMsg = ref("");
+const name = ref("");
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsg.value = await invoke("greet", { name: name.value });
+}
+function greet1() {
+  console.log(1111);
+}
+</script>
+
+<template>
+  <div class="card">
+    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+    <button type="button" @click="">Greet</button>
+  </div>
+
+  <p>{{ greetMsg }}</p>
+</template>
